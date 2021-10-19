402dea6a35c3be8d5995eb999d786c7176262096
Harshit Singh <hrshit2000@gmail.com>
2021-10-20 00:57:19 +0530

    edited settings.py

--- StudyBud/settings.py
+++ StudyBud/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 import django_heroku
+import decouple
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,15 +22,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^9=-zj@t08rp7vxtv+umcgm9g9=7v1z8(76q^-ybrwz=!slw+w'
+SECRET_KEY = decouple.config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ['mystudyroom2000.herokuapp.com',
                  'localhost']
-
 
 # Application definition
 
